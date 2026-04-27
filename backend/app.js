1c5d616ede0788bfa6f224af5d7aa29ec76440fa
menal18 <mexn142614@gmail.com>
2026-04-27 12:55:36 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { connectdb } from './config/db.js'
+import cors from "cors"
 import user from "./route/userroute.js"
 const app=express()
+
 app.use(express.json()) 
+app.use(cors())    
 connectdb()
 
 app.use("/api/",user)
